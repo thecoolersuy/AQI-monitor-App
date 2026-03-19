79ee423c55b49ddf8ca6468004e928db594aa5d4
oatkrunch <suyalsahukhala787@gmail.com>
2026-03-19 15:56:37 +0545

    AQI monitor and Weather Forcast App

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const weather = document.querySelector(".weather")
 const info = document.querySelector(".weatherInfo")
 const emoji = document.querySelector(".weatheremoji")
 const card = document.querySelector(".displayWeatherCard")
+const aqiCard= document.querySelector(".displayAQICard")
+// const indicators = document.querySelector(".indicatorValues")
 
 const apiKey_weather = "ba7145b1f075417c5b7241a664bf0382";
 const apiKey_AQI = "45dfac023f4d364d8b22b246a25a83c76149208d";
@@ -47,13 +49,14 @@ form.addEventListener("submit", async function(e){
             card.textContent=""
             card.style.display= "flex"
             card.style.flexDirection = "column"
-            card.style.gap = "2px"
+            card.style.gap = "10px"
             card.style.padding = "10px"
             card.style.width = "320px"
             card.style.height= "510px"
-            card.style.background = "linear-gradient(180deg , #fe5060, #feac46)"
-            card.style.border = "4px solid white"
-            card.style.borderRadius = "15px"
+            card.style.backgroundColor = "#ffffff"
+            card.style.border = "1px white"
+            card.style.borderRadius = "12px"
+            card.style.boxShadow = "0px 4px 15px rgba(0,0,0,0.1)"
 
 
            
@@ -113,4 +116,148 @@ form.addEventListener("submit", async function(e){
                 card.textContent = `${message}`
         }
         
+})
+
+form.addEventListener("submit", async (e) =>{
+     e.preventDefault()
+     const city = inputName.value
+
+     if(city){
+       try{
+          const AQI = await getAQI(city);
+          displayAQI(AQI)
+          
+
+       }catch(error){
+        console.log(error);
+       }
+     }
+     async function getAQI(city){
+            const response = await fetch(`https://api.waqi.info/feed/${city}/?token=${apiKey_AQI}`)
+            console.log(response);
+            const AQIdata = await response.json();
+            console.log(AQIdata)
+            return AQIdata;
+     }
+     
+   
+
+     async function displayAQI(AQI){
+          if(AQI.status == 'ok'){
+              const {data:{aqi, city:{ name },dominentpol}} = AQI
+              console.log(aqi)
+              console.log(dominentpol)
+              
+              
+              const displayAQIHeader = document.createElement("h1")
+              const displayEmotion = document.createElement("img")
+              const displayCity = document.createElement("p")
+              const displayAQI = document.createElement("p")
+              const displayPM = document.createElement("p")
+              const displayStatus = document.createElement("p")
+                   
+              
+              aqiCard.textContent = ""
+              aqiCard.style.display = "flex"
+              aqiCard.style.flexDirection = "column"
+              aqiCard.style.alignItems = "center"
+              aqiCard.style.padding = "20px"
+              aqiCard.style.gap = "12px"
+              aqiCard.style.backgroundColor = "#ffffff"
+              aqiCard.style.width = "220px"
+              aqiCard.style.height = "300px"
+              aqiCard.style.borderRadius = "10px"
+              aqiCard.style.boxShadow = "0px 4px 15px rgba(0,0,0,0.1)"
+
+              const indicators = document.createElement("div")
+              indicators.className = "indicatorValues"
+              
+
+              displayAQIHeader.textContent = "Air Quality Index (AQI)"
+              displayAQIHeader.className = "area"
+              displayAQIHeader.style.margin = "0px"
+
+              displayEmotion.src= `${displayAQIEmotion(aqi)}`
+              displayEmotion.alt = "image is only in the hosts computer so it wont display here"
+              displayEmotion.width = 100
+              displayEmotion.height = 100
+              displayEmotion.style.borderRadius = "10px"
+              
+
+               displayCity.textContent = `${city}`
+               displayCity.className = "area"
+               displayCity.style.margin = "0px"
+
+               displayAQI.textContent = `${aqi}`
+               displayAQI.className = "aqiValue"
+               displayAQI.style.margin = "0px"
+
+               displayStatus.textContent = `${showStatus(aqi)}`
+               displayStatus.className = "aqiInfo"
+               displayStatus.style.margin = "0px"
+
+               displayPM.textContent = `pollutant : ${dominentpol}`
+               displayPM.className = "aqiStatus"
+               displayPM.style.margin = "0px"
+
+              aqiCard.appendChild(displayAQIHeader)
+              aqiCard.appendChild(displayEmotion)
+              aqiCard.appendChild(displayCity)
+              const infoDisplay = aqiCard.appendChild(indicators)
+              infoDisplay.appendChild(displayAQI)
+              infoDisplay.appendChild(displayStatus)
+              aqiCard.appendChild(displayPM)
+               
+          }else{
+               displayError("Response is not okay")
+          }
+                   
+     }
+
+     function displayError(message){
+         aqiCard.textContent = `${message}`
+     }
+
+
+
+     function displayAQIEmotion(aqi){
+          switch(true){
+               case (aqi >= 0 && aqi <= 50 ):
+                  return "AQIemotions/0-50good.jpg";
+               case (aqi >= 51 && aqi <=100 ):
+                  return "AQIemotions/51-100moderate.jpg";
+               case (aqi >= 101 && aqi <= 150 ):
+                  return "AQIemotions/101-150Unhealthyforsensitivegroups.jpg";
+               case (aqi >= 151 && aqi <=200 ):
+                  return "AQIemotions/151-200Unhealthy.jpg";
+               case (aqi >= 201 && aqi <= 300 ):
+                  return "AQIemotions/201-300veryunhealthy.jpg";
+               case (aqi >= 301):
+                  return "AQIemotions/301-500hazardous.jpg";
+               default:
+                    return "aqi cannot be detected"
+          }
+     }
+
+     function  showStatus(aqi){
+          switch(true){
+               case(0 <= aqi <=50):
+                    return `• Good`
+               case(51 <= aqi <=100):
+                    return `• Moderate`
+               case(101<= aqi <=150):
+                    return `• Unhealthy for sensitive groups`
+               case(151 <= aqi <=200):
+                    return `• Unhealthy`
+               case(201 <= aqi <=300):
+                    return `• Very Unhealthy`
+               case(301 <= aqi):
+                    return `• Hazardous`
+               default:
+                    return `• unrecognized`
+          }
+     }
+     function displayAQITextColor(){
+
+     }
 })
